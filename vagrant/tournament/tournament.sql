a487dbea083b6b4bcc916f4f5542fbc1ab308071
Evan Genest <genest.1@osu.edu>
2017-11-09 15:15:17 -0600

    First commit.  Just creates one db, two tables called games, players.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -3,7 +3,22 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+-- Evan Genest  November 8, 2017
+-- Udacity: "Intro to Relational Databases"
+
+
+create database ashe;
+
+CREATE TABLE games (
+	playerID integer,
+	round integer,
+	game serial
+);
+
+CREATE TABLE players (
+	name text,
+	cumulativePts integer,
+	playerID serial	
+);
 
 
